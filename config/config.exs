e9dc7046ec99e0ed9acdd62ba7bbca3e95379d77
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-10-10 13:03:19 +0200

    chore: drop Mix.Config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,5 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+# This file is responsible for configuring your application and its dependencies.
+import Config
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
@@ -20,11 +19,3 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
